a9901f7432af8715ff981f0e73c330df675a6fc1
DanielCruz2906 <danielcruz75357@gmail.com>
2020-05-17 00:21:56 -0500

    formula v1

---
+++ elasticidad.py
@@ -0,0 +1,16 @@
+class Elasticidad:
+    def__init__(self,q1,q2,p1,p2):
+    self.__q1 = q1 
+    self.__q2 = q2
+    self.__p1 = p1
+    self.__p2 = p2
+
+def formula(self):
+    LeftUp = self.__q2 - self.__q1 
+    LeftDown = (self.__q1 + self.__q2) / 2
+    RightUp = self.__p2 - self.__p2
+    RightDown = (self.__p1 + self.__p2) / 2
+    
+    LeftWhole = Left up / LeftDown
+    RightWhole = RightUp / RightDown
+    Ed = LeftWhole / RightWhole
